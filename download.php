574332e5ce945cfcc4461e56d28ab5e9c78d779e
ahmadqsyaa <114840842+ahmadqsyaa@users.noreply.github.com>
2023-05-29 01:57:21 +0700

    Add files via upload

---
+++ download.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="id">
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>download</title>
+</head>
+
+<body style="text-align:center ;">
+   <img src="https://rickk.my.id/img/_20230529_014217.JPG" height="50%" width="50%"><br>
+  <a href="https://rickk.my.id/img/logo_smk1_pkllsng.pdf" download>Download</a>
+</body>
